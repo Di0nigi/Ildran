a3690fef0dfa587fbcc94eea2c1a5f88c5a84ffc
Di0nigi <dionigi.ex3@gmail.com>
2023-09-25 19:48:19 +0200

    first api comunication setup, not working

--- MainApp/src/App.jsx
+++ MainApp/src/App.jsx
@@ -1,3 +1,27 @@
-export default function App(){
-  return "hi"
+import React from "react";
+import { render } from "../../AppApi/app";
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={apiResponse:""};
+  }
+  callApi(){
+    fetch("http://localhost:3000/testApi")
+    .then(res=> res.text())
+    .then(res=>this.setState({apiResponse:res}));
+  }
+  componentWillMount(){
+    this.callApi();
+  }
+
+  render(){
+    console.log(this.state.apiResponse);
+    return this.state.apiResponse
+  }
 }
+
+
+
+
+export default App;
